6e44f81f93aaf77869cf867e179242ca05928133
Andrew Kim <20strategy@gmail.com>
2021-03-14 22:36:27 -0700

    favicon and footer updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,14 @@
 require('dotenv').config()
 const axios = require("axios")
 export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
+ 
   head: {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.io' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   /*
